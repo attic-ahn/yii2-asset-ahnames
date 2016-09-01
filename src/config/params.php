53528d144a17e77a999ad96b183acea63557de88
Andrii Vasyliev <sol@hiqdev.com>
2016-09-01 16:04:20 +0000

    fixed params, added social links

--- src/config/params.php
+++ src/config/params.php
@@ -10,8 +10,14 @@
  */
 
 return [
-    'orgName'      => 'AHnames',
-    'orgUrl'       => 'https://ahnames.com/',
-    'supportEmail' => 'support@ahnames.com',
-    'adminEmail'   => 'support@ahnames.com',
+    'organizationName'  => 'AHnames',
+    'organizationUrl'   => 'https://ahnames.com/',
+    'supportEmail'      => 'support@ahnames.com',
+    'adminEmail'        => 'support@ahnames.com',
+
+    'copyrightYear'     => 2011,
+
+    'twitter_link'      => 'https://twitter.com/ahnames',
+    'facebootk_link'    => 'https://www.facebook.com/ahnames',
+    'vk_link'           => 'https://vk.com/ahnames',
 ];
