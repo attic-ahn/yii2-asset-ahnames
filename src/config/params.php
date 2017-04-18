04915a6b6b2106aac3bb4ddd0cc52ca4338b7a85
Andrii Vasyliev <sol@hiqdev.com>
2017-04-18 15:46:13 +0200

    added favicon.ico param

--- src/config/params.php
+++ src/config/params.php
@@ -10,12 +10,16 @@
  */
 
 return [
+    'favicon.ico'       => '@ahnames/assets/ahnames/assets/images/favicon.ico',
+
     'organizationName'  => 'AHnames',
     'organizationUrl'   => 'https://ahnames.com/',
+
     'supportEmail'      => 'support@ahnames.com',
     'adminEmail'        => 'support@ahnames.com',
     'abuseEmail'        => 'abuse@ahnames.com',
     'salesEmail'        => 'sales@ahnames.com',
+
     'contactICQ'        => '593-341-721',
     'contactSkype'      => 'ah.andre',
 
